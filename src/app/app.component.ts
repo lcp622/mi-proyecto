958e41694ecd9117b9dde3af95751ee56ae1e014
lcp622 <luu_manzanita@hotmail.com>
2024-04-10 12:47:54 +0200

    Creo un nuevo componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PadreComponent } from "./padre/padre.component"; //lo que estoy poniendo aquí se llama igual que la clase PadreComponent
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, PadreComponent]
 })
 export class AppComponent {
 
