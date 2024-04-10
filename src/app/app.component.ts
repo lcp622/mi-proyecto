8e59ac1b0b1331fb7d964eeacaae8e5e51860ec8
lcp622 <luu_manzanita@hotmail.com>
2024-04-10 12:59:23 +0200

    Creación de componente Hijo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { PadreComponent } from "./padre/padre.component"; //lo que estoy poniendo aquí se llama igual que la clase PadreComponent
+import { PadreComponent } from "./padre/padre.component";
+import { HijoComponent } from './hijo/hijo.component'; //lo que estoy poniendo aquí se llama igual que la clase PadreComponent
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, PadreComponent]
+    imports: [RouterOutlet, PadreComponent, HijoComponent]
 })
 export class AppComponent {
 
--- src/app/padre/padre.component.ts
+++ src/app/padre/padre.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { HijoComponent } from "../hijo/hijo.component";
 
 @Component({
-  selector: 'app-padre',
-  standalone: true,
-  imports: [],
-  templateUrl: './padre.component.html',
-  styleUrl: './padre.component.css'
+    selector: 'app-padre',
+    standalone: true,
+    templateUrl: './padre.component.html',
+    styleUrl: './padre.component.css',
+    imports: [HijoComponent]
 })
 export class PadreComponent {
 
+    title :string = 'Esto es el título del padre';
 }
